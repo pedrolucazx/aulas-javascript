989135693ab37b771f5f72856e6b1acc3cafe512
Pedro Lucas <pedrolucastrainee@gmail.com>
2021-08-01 15:03:41 -0300

    novas aulas

---
+++ fundamentos/let1.js
@@ -0,0 +1,11 @@
+var numero = 1;
+{
+    {
+        let numero = 2
+        console.log('dentro = ', numero);
+    }
+}
+
+console.log('fora = ', numero);
+
+//! Diferença entre var e let, let tem espoco de funcão bloco e globa
--- fundamentos/var1.js
+++ fundamentos/var1.js
@@ -1,1 +1,21 @@
-dfsfsdfdasda
+{
+    {
+        {
+            var sera = 'Será????'
+            console.log(sera);
+        }
+    }
+}
+
+console.log(sera);
+
+function teste() {
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+//console.log(local);
+
+
+//! var todas as variaveis fora do escopo da função são globais
